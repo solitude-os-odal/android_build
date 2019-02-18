402fb45da8dd0ba9091851731de8279e9ab06558
Connor Hughes <connorhughes@fastmail.com>
2019-02-18 16:59:28 +0000

    Inclusion of solitude.mk to commit

---
+++ target/product/solitude.mk
@@ -0,0 +1,12 @@
+# Extra packages required for Solitude OS
+
+PRODUCT_PACKAGES += \
+    additional_repos \
+    FakeStore \
+    FDroid \
+    FDroidPrivilegedExtension \
+    GmsCore \
+    GsfProxy \
+    MozillaNlpBackend \
+    NominatimNlpBackend \
+    OpenWeatherMapWeatherProvider \
